f239b401cf23cfea1cfb1b0dd34dc904169b4733
ultramarshall <ugd24jam@gmail.com>
2017-04-28 12:36:52 +0700

    proteksi js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,77 +1,78 @@
-$(document).ready(function () {
-    $("#nav-button").on("click", function () {
-        $('#navigation:checkbox').change(function () {
-            // this will contain a reference to the checkbox   
-            if (this.checked) {
-                $('nav').css('height', '259px');
-            } else {
-                $('nav').css('height', '0px');
-            }
-        });
-
-    });
-    $(window).resize(function () {
-        if ($(this).width() >= 1025) {
-            $('nav').css({
-                'height': '30px',
-                'top': '0px',
-                'position': 'relative'
-            });
-        }
-        if ($(this).width() <= 1024) {
-            $('nav').css({
-                'height': '0px',
-                'top': '60px',
-                'width': '100%',
-                'position': 'absolute'
-            });
-        }
-    });
-
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll > 100) {
-            $('nav').css({
-                'top': '0px',
-                'position': 'fixed',
-                'width': '1024px',
-                'z-index': 2
-            });
-            $('.logo').css({
-                'z-index': 1,
-            });
-        } else {
-            $('nav').css({
-                'z-index': '0',
-                'position': 'relative',
-                'z-index': 2
-
-            });
-            $('.logo').css('z-index', '3');
-        }
-    });
-
-    $('#nav-daftar').on('click', function () {
-        //local link
-        $('section').load('/../partial/register.html');
-        //github link
-        $('section').load('/taruhan88/partial/register.html');
-        //$.getScript('/asset/js/register.js');
-    });
-
-    $('#nav-deposit').on('click', function () {
-        //local link
-        $('section').load('/../partial/deposit.html');
-        //github link
-        $('section').load('/taruhan88/partial/deposit.html');
-        //$.getScript('/asset/js/register.js');
-    });
-    $('#nav-games').on('click', function () {
-        $('.games-t88').css('display', 'block');
-    });
-    
-    $('.close-game').on('click', function(){
-        $('.games-t88').css('display', 'none');
-    });
-
-});
+//$(document).ready(function () {
+//    $("#nav-button").on("click", function () {
+//        $('#navigation:checkbox').change(function () {
+//            // this will contain a reference to the checkbox   
+//            if (this.checked) {
+//                $('nav').css('height', '259px');
+//            } else {
+//                $('nav').css('height', '0px');
+//            }
+//        });
+//
+//    });
+//    $(window).resize(function () {
+//        if ($(this).width() >= 1025) {
+//            $('nav').css({
+//                'height': '30px',
+//                'top': '0px',
+//                'position': 'relative'
+//            });
+//        }
+//        if ($(this).width() <= 1024) {
+//            $('nav').css({
+//                'height': '0px',
+//                'top': '60px',
+//                'width': '100%',
+//                'position': 'absolute'
+//            });
+//        }
+//    });
+//
+//    $(window).scroll(function () {
+//        var scroll = $(window).scrollTop();
+//        if (scroll > 100) {
+//            $('nav').css({
+//                'top': '0px',
+//                'position': 'fixed',
+//                'width': '1024px',
+//                'z-index': 2
+//            });
+//            $('.logo').css({
+//                'z-index': 1,
+//            });
+//        } else {
+//            $('nav').css({
+//                'z-index': '0',
+//                'position': 'relative',
+//                'z-index': 2
+//
+//            });
+//            $('.logo').css('z-index', '3');
+//        }
+//    });
+//
+//    $('#nav-daftar').on('click', function () {
+//        //local link
+//        $('section').load('/../partial/register.html');
+//        //github link
+//        $('section').load('/taruhan88/partial/register.html');
+//        //$.getScript('/asset/js/register.js');
+//    });
+//
+//    $('#nav-deposit').on('click', function () {
+//        //local link
+//        $('section').load('/../partial/deposit.html');
+//        //github link
+//        $('section').load('/taruhan88/partial/deposit.html');
+//        //$.getScript('/asset/js/register.js');
+//    });
+//    $('#nav-games').on('click', function () {
+//        $('.games-t88').css('display', 'block');
+//    });
+//    
+//    $('.close-game').on('click', function(){
+//        $('.games-t88').css('display', 'none');
+//    });
+//
+//});
+var _0x4627=["\x63\x6C\x69\x63\x6B","\x63\x68\x65\x63\x6B\x65\x64","\x68\x65\x69\x67\x68\x74","\x32\x35\x39\x70\x78","\x63\x73\x73","\x6E\x61\x76","\x30\x70\x78","\x63\x68\x61\x6E\x67\x65","\x23\x6E\x61\x76\x69\x67\x61\x74\x69\x6F\x6E\x3A\x63\x68\x65\x63\x6B\x62\x6F\x78","\x6F\x6E","\x23\x6E\x61\x76\x2D\x62\x75\x74\x74\x6F\x6E","\x77\x69\x64\x74\x68","\x33\x30\x70\x78","\x72\x65\x6C\x61\x74\x69\x76\x65","\x36\x30\x70\x78","\x31\x30\x30\x25","\x61\x62\x73\x6F\x6C\x75\x74\x65","\x72\x65\x73\x69\x7A\x65","\x73\x63\x72\x6F\x6C\x6C\x54\x6F\x70","\x66\x69\x78\x65\x64","\x31\x30\x32\x34\x70\x78","\x2E\x6C\x6F\x67\x6F","\x30","\x7A\x2D\x69\x6E\x64\x65\x78","\x33","\x73\x63\x72\x6F\x6C\x6C","\x2F\x2E\x2E\x2F\x70\x61\x72\x74\x69\x61\x6C\x2F\x72\x65\x67\x69\x73\x74\x65\x72\x2E\x68\x74\x6D\x6C","\x6C\x6F\x61\x64","\x73\x65\x63\x74\x69\x6F\x6E","\x2F\x74\x61\x72\x75\x68\x61\x6E\x38\x38\x2F\x70\x61\x72\x74\x69\x61\x6C\x2F\x72\x65\x67\x69\x73\x74\x65\x72\x2E\x68\x74\x6D\x6C","\x23\x6E\x61\x76\x2D\x64\x61\x66\x74\x61\x72","\x2F\x2E\x2E\x2F\x70\x61\x72\x74\x69\x61\x6C\x2F\x64\x65\x70\x6F\x73\x69\x74\x2E\x68\x74\x6D\x6C","\x2F\x74\x61\x72\x75\x68\x61\x6E\x38\x38\x2F\x70\x61\x72\x74\x69\x61\x6C\x2F\x64\x65\x70\x6F\x73\x69\x74\x2E\x68\x74\x6D\x6C","\x23\x6E\x61\x76\x2D\x64\x65\x70\x6F\x73\x69\x74","\x64\x69\x73\x70\x6C\x61\x79","\x62\x6C\x6F\x63\x6B","\x2E\x67\x61\x6D\x65\x73\x2D\x74\x38\x38","\x23\x6E\x61\x76\x2D\x67\x61\x6D\x65\x73","\x6E\x6F\x6E\x65","\x2E\x63\x6C\x6F\x73\x65\x2D\x67\x61\x6D\x65","\x72\x65\x61\x64\x79"];$(document)[_0x4627[40]](function(){$(_0x4627[10])[_0x4627[9]](_0x4627[0],function(){$(_0x4627[8])[_0x4627[7]](function(){if(this[_0x4627[1]]){$(_0x4627[5])[_0x4627[4]](_0x4627[2],_0x4627[3])}else {$(_0x4627[5])[_0x4627[4]](_0x4627[2],_0x4627[6])}})});$(window)[_0x4627[17]](function(){if($(this)[_0x4627[11]]()>= 1025){$(_0x4627[5])[_0x4627[4]]({"\x68\x65\x69\x67\x68\x74":_0x4627[12],"\x74\x6F\x70":_0x4627[6],"\x70\x6F\x73\x69\x74\x69\x6F\x6E":_0x4627[13]})};if($(this)[_0x4627[11]]()<= 1024){$(_0x4627[5])[_0x4627[4]]({"\x68\x65\x69\x67\x68\x74":_0x4627[6],"\x74\x6F\x70":_0x4627[14],"\x77\x69\x64\x74\x68":_0x4627[15],"\x70\x6F\x73\x69\x74\x69\x6F\x6E":_0x4627[16]})}});$(window)[_0x4627[25]](function(){var _0x43b8x1=$(window)[_0x4627[18]]();if(_0x43b8x1> 100){$(_0x4627[5])[_0x4627[4]]({"\x74\x6F\x70":_0x4627[6],"\x70\x6F\x73\x69\x74\x69\x6F\x6E":_0x4627[19],"\x77\x69\x64\x74\x68":_0x4627[20],"\x7A\x2D\x69\x6E\x64\x65\x78":2});$(_0x4627[21])[_0x4627[4]]({"\x7A\x2D\x69\x6E\x64\x65\x78":1})}else {$(_0x4627[5])[_0x4627[4]]({"\x7A\x2D\x69\x6E\x64\x65\x78":_0x4627[22],"\x70\x6F\x73\x69\x74\x69\x6F\x6E":_0x4627[13],"\x7A\x2D\x69\x6E\x64\x65\x78":2});$(_0x4627[21])[_0x4627[4]](_0x4627[23],_0x4627[24])}});$(_0x4627[30])[_0x4627[9]](_0x4627[0],function(){$(_0x4627[28])[_0x4627[27]](_0x4627[26]);$(_0x4627[28])[_0x4627[27]](_0x4627[29])});$(_0x4627[33])[_0x4627[9]](_0x4627[0],function(){$(_0x4627[28])[_0x4627[27]](_0x4627[31]);$(_0x4627[28])[_0x4627[27]](_0x4627[32])});$(_0x4627[37])[_0x4627[9]](_0x4627[0],function(){$(_0x4627[36])[_0x4627[4]](_0x4627[34],_0x4627[35])});$(_0x4627[39])[_0x4627[9]](_0x4627[0],function(){$(_0x4627[36])[_0x4627[4]](_0x4627[34],_0x4627[38])})})
